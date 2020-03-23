7a0cb63d6fc29ca7401000bb5dda08bfa9ba87ae
irina08 <38026611+irina08@users.noreply.github.com>
2020-03-22 22:42:48 -0700

    change for e.printStackTrace()

--- src/test/java/appium/utils/AppStart.java
+++ src/test/java/appium/utils/AppStart.java
@@ -16,70 +16,68 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class AppStart {
 
-	private static AppConfig appConfig = new AppConfig();
-	private static Properties properties = new Properties();
-	private static AppiumDriver<MobileElement> driver;
+    private static AppConfig appConfig = new AppConfig();
+    private static Properties properties = new Properties();
+    private static AppiumDriver<MobileElement> driver;
 
-	static {
+    static {
+	properties = readConfigFile(Constants.CONFIG_FILE_PATH);
+	appConfig.setPlatformName(properties.getProperty("platformName"));
+	appConfig.setPlatformVersion(properties.getProperty("platformVersion"));
+	appConfig.setDeviceName(properties.getProperty("deviceName"));
+	appConfig.setApp(properties.getProperty("app"));
+	appConfig.setAppiumServer(properties.getProperty("appiumServer"));
+	appConfig.setApiKey(properties.getProperty("apiKey"));
+	appConfig.setTimeout(properties.getProperty("timeout"));
+    }
 
-		properties = readConfigFile(Constants.CONFIG_FILE_PATH);
-		appConfig.setPlatformName(properties.getProperty("platformName"));
-		appConfig.setPlatformVersion(properties.getProperty("platformVersion"));
-		appConfig.setDeviceName(properties.getProperty("deviceName"));
-		appConfig.setApp(properties.getProperty("app"));
-		appConfig.setAppiumServer(properties.getProperty("appiumServer"));
-		appConfig.setApiKey(properties.getProperty("apiKey"));
-		appConfig.setTimeout(properties.getProperty("timeout"));
+    public static AppiumDriver<MobileElement> startApp() throws MalformedURLException {
+	DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    
+	desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, appConfig.getPlatformName());
+	desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, appConfig.getPlatformVersion());
+	desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, appConfig.getDeviceName());
+	desiredCapabilities.setCapability(MobileCapabilityType.APP, appConfig.getApp());
+	desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, appConfig.getTimeout());
+	desiredCapabilities.setCapability(Constants.KEYBOARD_SETTING, false);
+
+	URL url = new URL(appConfig.getAppiumServer());
+
+	if (appConfig.getPlatformName().equalsIgnoreCase(Constants.IOS_PLATFORM_NAME)) {
+	    desiredCapabilities.setCapability(Constants.AUTOMATION_NAME, Constants.AUTOMATION_VALUE);
+	    driver = new IOSDriver<MobileElement>(url, desiredCapabilities);
 	}
+	driver.manage().timeouts().implicitlyWait(Long.valueOf(appConfig.getTimeout()), TimeUnit.SECONDS);
 
-	public static AppiumDriver<MobileElement> startApp() throws MalformedURLException {
+	return driver;
+    }
 
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    public static String getAPiKey() {
+	return appConfig.getApiKey();
+    }
 
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, appConfig.getPlatformName());
-		desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, appConfig.getPlatformVersion());
-		desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, appConfig.getDeviceName());
-		desiredCapabilities.setCapability(MobileCapabilityType.APP, appConfig.getApp());
-		desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, appConfig.getTimeout());
-		desiredCapabilities.setCapability(Constants.KEYBOARD_SETTING, false);
-
-		URL url = new URL(appConfig.getAppiumServer());
-
-		if (appConfig.getPlatformName().equalsIgnoreCase(Constants.IOS_PLATFORM_NAME)) {
-		    desiredCapabilities.setCapability(Constants.AUTOMATION_NAME, Constants.AUTOMATION_VALUE);
-		    driver = new IOSDriver<MobileElement>(url, desiredCapabilities);
-		}
-		driver.manage().timeouts().implicitlyWait(Long.valueOf(appConfig.getTimeout()), TimeUnit.SECONDS);
-
-		return driver;
-	}
-
-	public static String getAPiKey() {
-		return appConfig.getApiKey();
-	}
-
-	public static String getTimeout() {
-		return appConfig.getTimeout();
-	}
+    public static String getTimeout() {
+	return appConfig.getTimeout();
+    }
 	
-	public static Properties readConfigFile(String file) {
-		InputStream input = null;
-		Properties properties = new Properties();
-		try {
-			input = new FileInputStream(file);
-			properties.load(input);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					System.out.println(e.getMessage());
-				}
+    public static Properties readConfigFile(String file) {
+	InputStream input = null;
+	Properties properties = new Properties();
+	try {
+		input = new FileInputStream(file);
+		properties.load(input);
+	} catch (IOException e) {
+		e.printStackTrace();		
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();	
 			}
 		}
-		return properties;
 	}
+	return properties;
+    }
 
 }
